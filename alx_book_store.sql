ad8a5bd0f7320ebfcf11a20c8c475efb2ed58460
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-05 10:44:56 +0000

    second attempt

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -43,8 +43,7 @@ CREATE TABLE Orders(
     PRIMARY KEY (order_id),
 
     CONSTRAINT fk_orders_customers
-        FOREIGN KEY (customer_id)
-        REFERENCES Customers(customer_id)
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
